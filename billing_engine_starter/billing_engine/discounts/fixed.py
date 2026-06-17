2c0405c750a0c34fec6c4bc96e5fa002d031b1d5
swastik kumar mahapatra <swastikmahapatra183@gmail.com>
2026-06-17 08:26:22 +0530

    Implement FixedAmountDiscount class methods

--- billing_engine_starter/billing_engine/discounts/fixed.py
+++ billing_engine_starter/billing_engine/discounts/fixed.py
@@ -11,9 +11,17 @@ from billing_engine.discounts.base import Discount, DiscountContext
 
 class FixedAmountDiscount(Discount):
     def __init__(self, amount: Money) -> None:
-        # TODO Day 1
         raise NotImplementedError("Day 1: implement FixedAmountDiscount.__init__")
 
     def apply(self, subtotal: Money, context: DiscountContext) -> Money:
-        # TODO Day 1
         raise NotImplementedError("Day 1: implement FixedAmountDiscount.apply")
+from billing_engine.money import Money
+from billing_engine.discounts.base import Discount, DiscountContext
+
+
+class FixedAmountDiscount(Discount):
+    def __init__(self, amount: Money) -> None:
+        self.amount = amount
+
+    def apply(self, subtotal: Money, context: DiscountContext) -> Money:
+        return self.amount if self.amount <= subtotal else subtotal
